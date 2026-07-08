eea958559f0621dee4104a777ae84649904e19dc
Yerko Flores Mamani <toxicusfumo@gmail.com>
2026-07-07 20:14:45 -0500

    fix:correccion configuración de TypeORM y conexión a la base de datos

--- proyecto-portafolio/src/app.module.ts
+++ proyecto-portafolio/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    //Configuracion para la conexión a PostgreSQL.
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],//Le dice a TypeORM dónde buscar las entidades.
+        synchronize: true, // ⚠️ Solo en desarrollo, para que cree las tablas solas
+        logging: true,     // Para que veas las consultas SQL en consola (te hace sentir hacker)
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
